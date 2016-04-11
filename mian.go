20162b87efe2e1a7afafa4b601d71bd19a91c37c
limengwei <892642257@qq.com>
2016-04-11 19:03:21 +0800

    merge

--- mian.go
+++ mian.go
@@ -15,7 +15,9 @@ func main() {
 
 	authorized := r.Group("/", interceptor)
 	authorized.GET("/", index)
+	authorized.GET("/art/:cate", art)
 	authorized.Any("/editor", editor)
+	authorized.Any("/sp", spider)
 
 	r.GET("/logout", logout)
 	r.Any("/login", login)
@@ -23,6 +25,7 @@ func main() {
 	r.Run(":80")
 }
 
+//拦截器
 func interceptor(c *gin.Context) {
 	cookie, err := c.Cookie("cookieee")
 
@@ -36,6 +39,7 @@ func interceptor(c *gin.Context) {
 
 }
 
+//登录
 func login(c *gin.Context) {
 	if c.Request.Method == "POST" {
 		if c.PostForm("name") == "admin" && c.PostForm("pwd") == "admin" {
@@ -55,25 +59,25 @@ func login(c *gin.Context) {
 
 var arts []string = []string{"AAA", "BBB", "CCC", "CCC", "CCC", "CCC", "CCC"}
 
+//首页（最新文章）
 func index(c *gin.Context) {
-
-	var ip, l, r int
-
-	p, _ := c.GetQuery("p")
-
-	ip, _ = strconv.Atoi(p)
-
-	l = ip - 1
-	r = ip + 1
-
-	if l < 1 {
-		l = 1
-		r = 2
-	}
+	l, r := getPagetOption(c)
+	cate := c.Params.ByName("cate")
+	fmt.Println("--cate--", cate)
 
 	c.HTML(200, "index.html", gin.H{"titles": arts, "l": l, "r": r})
 }
 
+//文章（分类文章）
+func art(c *gin.Context) {
+	l, r := getPagetOption(c)
+	cate := c.Params.ByName("cate")
+	fmt.Println("--cate--", cate)
+
+	c.HTML(200, "index.html", gin.H{"titles": arts, "l": l, "r": r})
+}
+
+//编辑
 func editor(c *gin.Context) {
 	if c.Request.Method == "POST" {
 		fmt.Println("title:", c.PostForm("title"))
@@ -92,9 +96,9 @@ func editor(c *gin.Context) {
 func setCookie(c *gin.Context) {
 	cookiename := "cookieee"
 	cookieval := "1024"
-	maxAge := 1024
+	maxAge := 102400
 	path := ""
-	domain := "localhost"
+	domain := "192.168.1.18"
 	secure := false
 	httpOnly := false
 	c.SetCookie(cookiename, cookieval, maxAge, path, domain, secure, httpOnly)
@@ -104,9 +108,9 @@ func setCookie(c *gin.Context) {
 func logout(c *gin.Context) {
 	cookiename := "cookieee"
 	cookieval := ""
-	maxAge := 1024
+	maxAge := 102400
 	path := ""
-	domain := "localhost"
+	domain := "192.168.1.18"
 	secure := false
 	httpOnly := false
 	c.SetCookie(cookiename, cookieval, maxAge, path, domain, secure, httpOnly)
@@ -114,7 +118,25 @@ func logout(c *gin.Context) {
 	c.Redirect(302, "/login")
 }
 
-//
+//分页
+func getPagetOption(c *gin.Context) (int, int) {
+
+	var ip, l, r int
+	p, _ := c.GetQuery("p")
+
+	ip, _ = strconv.Atoi(p)
+
+	l = ip - 1
+	r = ip + 1
+
+	if l < 1 {
+		l = 1
+		r = 2
+	}
+	return l, r
+}
+
+//404
 func notfound(c *gin.Context) {
 	c.HTML(404, "404.html", nil)
 }
