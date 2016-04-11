03b02a4ca7d490b7fea52f91d271b03aebd315b4
limengwei <892642257@qq.com>
2016-04-11 19:02:50 +0800

    网页爬取 Signed-off-by: limengwei <892642257@qq.com>

---
+++ spider.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gin-gonic/gin"
+)
+
+//爬虫
+func spider(c *gin.Context) {
+	if c.Request.Method == "GET" {
+		c.HTML(200, "spider.html", nil)
+	}
+
+	if c.Request.Method == "POST" {
+		url, exist := c.GetPostForm("urll")
+		if exist {
+			fmt.Println(url)
+			crawl(url, c)
+		} else {
+			c.HTML(200, "spider.html", nil)
+		}
+	}
+
+}
+
+var ch chan int
+
+func crawl(url string, c *gin.Context) {
+	u_base := "http://www.cnblogs.com/#p"
+	for i := 1; i < 2; i++ {
+		u_pager := u_base + strconv.Itoa(i)
+		crawlList(u_pager, c)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+//分页
+var a article
+
+func crawlList(url string, c *gin.Context) {
+
+	var titles []string
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	selection := doc.Find("#post_list").First().Find(".post_item")
+	ch = make(chan int, selection.Size())
+
+	selection.Each(func(i int, s *goquery.Selection) {
+		time.Sleep(1 * time.Second)
+		title := s.Find(".titlelnk").First()
+		summary := s.Find(".post_item_summary").First()
+		titles = append(titles, title.Text())
+		detailUrl, exists := title.Attr("href")
+
+		if exists {
+			//crawlDetail(detailUrl, c)
+			a = article{}
+			a.title = title.Text()
+			a.body = summary.Text()
+
+			insert(a)
+		}
+
+		fmt.Printf("【%d----%s----%s】\n", i, title.Text(), detailUrl)
+
+	})
+
+	//c.HTML(200, "spider.html", gin.H{"titles": titles})
+}
+
+//解析文章内容
+func crawlDetail(url string, c *gin.Context) {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	body := doc.Find("#cnblogs_post_body").First().Text()
+
+	rs := []rune(body)
+	fmt.Println(string(rs[0:20])) //字符串截取
+}
